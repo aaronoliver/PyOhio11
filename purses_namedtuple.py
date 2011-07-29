491bc9bb0c49bed8636b6679789fe10330018ec2
Aaron Oliver <aaronoliver@aaronoliver.com>
2011-07-28 21:43:34 -0500

    take out old line

--- purses_namedtuple.py
+++ purses_namedtuple.py
@@ -8,9 +8,7 @@ from collections import namedtuple
 Purse = namedtuple('Purse', 'sku name price')
 
 data = json.load(open('purses.json'))
-#purses = [Purse(p['sku'], p['name'], p['price']) for p in data['purses']]
 purses = [Purse(**p) for p in data['purses']]
-
 
 for purse in purses:
     if not purse.price:
